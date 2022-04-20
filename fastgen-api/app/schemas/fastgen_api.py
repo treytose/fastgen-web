2b21cee72cca40dd5be66339a922fd07ed6efecd
treytose <treyholthe@gmail.com>
2022-04-20 00:32:06 +0000

    updated fastgen_api schema

--- fastgen-api/app/schemas/fastgen_api.py
+++ fastgen-api/app/schemas/fastgen_api.py
@@ -3,10 +3,10 @@ from fastapi import Query
 from typing import Optional
 
 class Fastgen_apiModel(BaseModel):          
-    name: str 
-    path: str
-    pythonVersion: str
-    dbType: Optional[str] = Query(None, title="Database Type")
-    dbName: str
-    dbUser: str
-    dbPass: str    
+    name: str = Query(None, title="Name", description="API Name") 
+    path: str = Query(None, title="Path", description="Absolute path to API") 
+    pythonVersion: Optional[str] = Query('python3.9', title="Python Version", description="The version of Python to create the API with") 
+    dbType: Optional[str] = Query('sqlite', title="Database Type", description="The database type to use") 
+    dbName: Optional[str] = Query('fast.db', title="Database Name", description="The name of the database or file") 
+    dbUser: Optional[str] = Query(None, title="Database User", description="The database user") 
+    dbPass: Optional[str] = Query(None, title="Database Password", description="The database password")
