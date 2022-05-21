6e0d99b7f4a780fd2828dccd85daed6d18c931ec
= <olholthe@ptci.com>
2022-05-20 20:16:13 -0500

    added support for search and sorting list of entities

--- util/codeGenerator.ts
+++ util/codeGenerator.ts
@@ -1,10 +1,27 @@
 import { EColumn } from "../components/entity/EntityColumn";
 import { toTitle } from ".";
 
+// helper function
+function indent_and_join(list: string[], spaces = 4) {
+  let s = "";
+  list.forEach((item) => {
+    let line = "";
+    for (var i = 0; i < spaces; i++) {
+      line += " ";
+    }
+    line += item.trim();
+    line += "\n";
+    s += line;
+  });
+
+  return s;
+}
+
 export default function generateCode(entity: string, columns: EColumn[]) {
   const pkName = `${entity}id`;
   const libName = `o${toTitle(entity)}`;
   const modelName = `${toTitle(entity)}Model`;
+  const joinedModelName = `${toTitle(entity)}JoinModel`;
   const className = toTitle(entity);
 
   function generate_fk_routes() {
@@ -67,6 +84,37 @@ export default function generateCode(entity: string, columns: EColumn[]) {
     return methods;
   }
 
+  function generate_join_statements() {
+    let joins: string[] = [];
+    let vars: string[] = [];
+
+    columns.forEach((ecolumn) => {
+      if (!ecolumn.fk || !ecolumn.fk.includes(".")) {
+        return;
+      }
+
+      let [name, col] = ecolumn.fk.split(".");
+
+      if (ecolumn.fkType === "many2many" || ecolumn.fkType === "one2many") {
+        joins.push(
+          `${name} = await self.get_${entity}_${name}s(${entity}.${ecolumn.name})`
+        );
+        vars.push(name);
+      } else if (ecolumn.fkType === "one2one") {
+        joins.push(
+          `${name} = await self.get_${entity}_${name}(${entity}.${ecolumn.name})`
+        );
+        vars.push(name);
+      }
+    });
+
+    return `${indent_and_join(joins, 12)}
+            return ${joinedModelName} (${
+      vars.length > 0 ? vars.map((v) => `${v}=${v}`).join(",") + "," : ""
+    } **${entity})
+    `;
+  }
+
   const mainCode = `
       from .routers import ${entity}
 
@@ -78,20 +126,20 @@ export default function generateCode(entity: string, columns: EColumn[]) {
       from app.libraries.lib${entity} import ${className}
       from app.schemas.${entity} import ${modelName}
 
-      router = APIRouter()
+      router = APIRouter(tags=["${entity}"])
       ${libName} = ${className}()
 
       @router.get("/${entity}/schema")
-      async def get_${entity}_schema():              
-        return ${libName}.get_${entity}_schema()        
+      async def get_${entity}_schema(joined: bool = False):              
+        return await ${libName}.get_${entity}_schema(joined=joined)        
 
       @router.get("/${entity}")
-      async def get_${entity}_list(limit: int = 100):            
-          return await ${libName}.get_${entity}_list(limit)
+      async def get_${entity}_list(joined: bool = False, limit: int = 100, offset: int = 0, sortField: str = None, sortOrder: str = "asc", search: str = ""):
+          return await ${libName}.get_${entity}_list(joined=joined, limit=limit, offset=offset, sortField=sortField, sortOrder=sortOrder, search=search)
 
       @router.get("/${entity}/{${pkName}}")
-      async def get_${entity}(${pkName}: int):
-          return await ${libName}.get_${entity}(${pkName})
+      async def get_${entity}(${pkName}: int, joined: bool = False):
+          return await ${libName}.get_${entity}(${pkName}, joined=joined)
 
       @router.post("/${entity}")
       async def create_${entity}(${entity}: ${modelName}):
@@ -109,27 +157,64 @@ export default function generateCode(entity: string, columns: EColumn[]) {
     `;
 
   const libCode = `
+    from http.client import HTTPException
     from app import db
-    from app.schemas.${entity} import ${modelName}
+    from app.schemas.${entity} import ${modelName}, ${joinedModelName}
 
     class ${className}:
+        async def __join_${entity}__(self, ${entity}):
+${generate_join_statements()}
+
         async def generate(self):
             await db.create_schema("${entity}", ${modelName}.schema())
 
-        async def get_${entity}_schema(self):
-            schema = ${modelName}.schema()
+        async def get_${entity}_schema(self, joined: bool = False):
+            schema = ${joinedModelName}.schema() if joined else ${modelName}.schema()
             for v in schema['properties'].values():
                 allowed_values = v.get("form_options", {}).get("allowed_values")
                 if allowed_values and isinstance(allowed_values, str) and str.startswith(allowed_values.upper(), "SELECT"):                                
                     v["form_options"]["allowed_values"] = await db.fetchall(allowed_values)                            
             return schema
 
-        async def get_${entity}_list(self, limit: int = 100):
-            ${entity} = await db.fetchall("SELECT * FROM ${entity} LIMIT :limit", {"limit": limit})
-            return ${entity}
+        async def get_${entity}_list(self, joined: bool = False, limit: int = 100, offset: int = 0, sortField: str = None, sortOrder: str = 'asc', search: str = ''): 
+            sortSql = ""
+            searchSql = ""
+            injectObject = {}
+    
+            schema = await self.get_${entity}_schema()
+    
+            if sortField:                        
+                sortOrder = 'desc' if sortOrder.lower() == 'asc' else 'asc'
+                if not sortField in schema['properties'].keys():
+                    raise HTTPException(status_code=400, detail='Invalid sortField')                
+                sortSql = f"ORDER BY {sortField} {sortOrder}"
+    
+            if search:
+                searchSql = 'WHERE '
+                searchItems = []
+                for name in schema['properties'].keys():
+                    searchItems.append(f'{name} LIKE :search_{name}')
+                    injectObject[f"search_{name}"] = f'%{search}%'
+    
+                searchSql += " OR ".join(searchItems)                                
+                        
+            ${entity}_list = await db.fetchall(f"SELECT * FROM ${entity} {searchSql} {sortSql} LIMIT {offset}, {limit}", injectObject)
+    
+            total_count = await db.fetchone("SELECT count(*) as count FROM ${entity}")
+            meta = {
+                "total_count": total_count["count"]
+            }
+    
+            if not joined:
+                return {"meta": meta, "data": ${entity}_list}
+    
+            ${entity}_list = [await self.__join_${entity}__(item) for item in ${entity}_list]
+            return {"meta": meta, "data": ${entity}_list}
 
-        async def get_${entity}(self, ${pkName}: int):
+        async def get_${entity}(self, ${pkName}: int, joined: bool = False):
             ${entity} = await db.fetchone("SELECT * FROM ${entity} WHERE ${pkName}=:${pkName}", {"${pkName}": ${pkName}})
+            if joined:
+                ${entity} = self.__join_${entity}__(${entity})
             return ${entity}
 
         async def create_${entity}(self, ${entity}: ${modelName}):
@@ -170,6 +255,36 @@ export default function generateCode(entity: string, columns: EColumn[]) {
     }
     return c.defaultValue;
   };
+
+  function generate_joined_schema() {
+    const joins: string[] = [];
+
+    columns.forEach((ecolumn) => {
+      if (!ecolumn.fk || !ecolumn.fk.includes(".")) {
+        return;
+      }
+
+      let [name, col] = ecolumn.fk.split(".");
+
+      joins.push(`${name}: ${toTitle(name)}JoinModel = None`);
+    });
+
+    return indent_and_join(joins, 12) || "            pass";
+  }
+
+  function generate_schema_imports() {
+    let imports: string[] = [];
+    columns.forEach((ecolumn) => {
+      if (!ecolumn.fk || !ecolumn.fk.includes(".")) {
+        return;
+      }
+
+      let [name, col] = ecolumn.fk.split(".");
+
+      imports.push(`from .${name} import ${toTitle(name)}JoinModel`);
+    });
+    return indent_and_join(imports, 6);
+  }
 
   function format_schema_column(c: EColumn) {
     let columnString = `${c.name}: `;
@@ -239,10 +354,15 @@ export default function generateCode(entity: string, columns: EColumn[]) {
   const schemaCode = `
       from pydantic import BaseModel
       from fastapi import Query
-      from typing import Optional
+      from typing import Optional    
+${generate_schema_imports()}  
       ${needsDatetimeImport ? "import datetime\n" : ""}
       class ${modelName}(BaseModel):          
           ${types}
+
+      class ${joinedModelName}(${modelName}):
+${generate_joined_schema()}
+          
     `;
 
   const sqlCode = `
